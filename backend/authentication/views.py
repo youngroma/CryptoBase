5b7279f29aee3536c0fe9b695f273ac793115a24
youngroma <krivichroma228@gmail.com>
2025-03-15 19:47:47 +0100

    Add views for registration, login, logout and profile

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        user = authenticate(email=email, password=password)
+        if user is None:
+            return Response({'error': 'Invalid email or password'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        access_token = RefreshToken.for_user(user).access_token
+        return Response({
+            'access': str(access_token),
+            'user': UserSerializer(user).data
+        })
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        return Response({'message': 'You have successfully logged out'}, status=status.HTTP_205_RESET_CONTENT)
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
