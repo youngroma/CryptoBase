209ed7df509047dae25d7d2107466e069e930b1a
youngroma <krivichroma228@gmail.com>
2025-04-28 19:30:25 +0200

    Implemented caching for storing historical cryptocurrency prices

--- backend/portfolio/services.py
+++ backend/portfolio/services.py
@@ -1,8 +1,20 @@
-# services.py
+import json
 import httpx
+from django.core.cache import cache
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 async def getChartDataAsync(slug, interval_type="daily"):
+    cache_key = f"chart_data_{slug}_{interval_type}"
+    cached_data = cache.get(cache_key)
+
+    if cached_data:
+        logging.info(f"Cache hit for {cache_key}")
+        return json.loads(cached_data)
+
+    logging.info(f"Cache miss for {cache_key}, fetching new data.")
+
     interval_days_map = {
         "5min": 1,
         "hourly": 30,
@@ -24,6 +36,11 @@ async def getChartDataAsync(slug, interval_type="daily"):
         data = response.json()
         prices = data.get("prices", [])
         formatted_data = [{"timestamp": ts, "price": price} for ts, price in prices]
+
+        print(f"Caching data for {cache_key}: {formatted_data}")
+        cache.set(cache_key, json.dumps(formatted_data), timeout=60 * 60)
+
+        logging.info("Fetched new data from API and cached it.")
         return formatted_data
     else:
         return {"error": response.json()}
