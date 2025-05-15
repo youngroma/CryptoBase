5d6aa0ee812b40942e1b23e95d1cc133353a00fa
YevhenK18 <yevheniikaliush@gmail.com>
2025-05-15 12:27:22 +0200

    Solving registration problems

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -6,7 +6,7 @@ const LoginModal = ({ show, onHide }) => {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const response = await fetch("http://127.0.0.1:8000/auth/login/", {
+    const response = await fetch("http://localhost:8000/auth/login/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
