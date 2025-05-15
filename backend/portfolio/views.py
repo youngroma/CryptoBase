3c718cb1b205c70771faa97c7f2954fd0d389f54
YevhenK18 <yevheniikaliush@gmail.com>
2025-05-15 12:47:11 +0200

    bug fix

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -65,7 +65,31 @@ class CryptoDetailView(APIView):
     def get(self, request, slug):
         cache_key = f"crypto_detail_{slug}"
         url = f"https://api.coingecko.com/api/v3/coins/{slug}"
+<<<<<<< Updated upstream
         data = get_cached_data(cache_key, url)
+=======
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            result = {
+                "name": data.get("name"),
+                "image": data["image"]["thumb"],
+                "symbol": data.get("symbol"),
+                "current_price": data["market_data"]["current_price"]["usd"],
+                "rank": data.get("market_cap_rank", "N/A"),  # Rating on CoinGecko
+                "price_change_percentage_24h": data["market_data"].get("price_change_percentage_24h", "N/A"),   # Change in 24 hours
+                "market_cap": data["market_data"]["market_cap"]["usd"],
+                "total_volume": data["market_data"]["total_volume"]["usd"],
+                "total_supply": data["market_data"].get("total_supply", "N/A"),  # may be none
+                "max_supply": data["market_data"].get("max_supply", "N/A"),  # may be none
+                "circulating_supply": data["market_data"].get("circulating_supply", "N/A"),  # may be none
+                "fdv": data["market_data"].get("fully_diluted_valuation", {}).get("usd", "N/A"),
+                "high_24h": data["market_data"].get("high_24h", {}).get("usd", "N/A"),  # Lowest
+                "low_24h": data["market_data"].get("low_24h", {}).get("usd", "N/A"),     # And Highest price in 24h
+                "ath": data["market_data"].get("ath", {}).get("usd", "N/A"),    # The Highest price of all time
+            
+            }
+>>>>>>> Stashed changes
 
         if "error" in data:
             return Response(data, status=status.HTTP_502_BAD_GATEWAY)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // ❌ Убрал BrowserRouter
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
