9f8524d6bee9872fb8b89819c23e59ef1f9050c3
youngroma <krivichroma228@gmail.com>
2025-05-23 18:16:29 +0200

    Implement model for Portfolio

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class PortfolioTransaction(models.Model):
+    TRANSACTION_TYPES = (
+        ("buy", "Buy"),
+        ("sell", "Sell"),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    coin_id = models.CharField(max_length=100)  # ID with CoinGecko, for example "bitcoin
+    symbol = models.CharField(max_length=10)
+    amount = models.DecimalField(max_digits=20, decimal_places=8)  # How much bought/sold
+    price_usd = models.DecimalField(max_digits=20, decimal_places=8)  # purchase price
+    type = models.CharField(max_length=4, choices=TRANSACTION_TYPES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.type} {self.amount} {self.coin_id}"
