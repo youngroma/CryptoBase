3635499bad19956326ceaca769dac903b3a80fe5
youngroma <krivichroma228@gmail.com>
2025-06-28 19:55:24 +0200

    Updated CryptoDetailView: add safe version of data extraction from nested dictionaries

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -77,20 +77,20 @@ class CryptoDetailView(APIView):
 
         result = {
             "name": data.get("name"),
-            "image": data["image"]["thumb"],
+            "image": data.get("image", {}).get("thumb"),
             "symbol": data.get("symbol"),
-            "current_price": data["market_data"]["current_price"]["usd"],
+            "current_price": data.get("market_data", {}).get("current_price", {}).get("usd", "N/A"),
             "rank": data.get("market_cap_rank", "N/A"),  # Rating on CoinGecko
-            "price_change_percentage_24h": data["market_data"].get("price_change_percentage_24h", "N/A"),   # Change in 24 hours
-            "market_cap": data["market_data"]["market_cap"]["usd"],
-            "total_volume": data["market_data"]["total_volume"]["usd"],
-            "total_supply": data["market_data"].get("total_supply", "N/A"),  # may be none
-            "max_supply": data["market_data"].get("max_supply", "N/A"),  # may be none
-            "circulating_supply": data["market_data"].get("circulating_supply", "N/A"),  # may be none
-            "fdv": data["market_data"].get("fully_diluted_valuation", {}).get("usd", "N/A"),
-            "high_24h": data["market_data"].get("high_24h", {}).get("usd", "N/A"),  # Lowest
-            "low_24h": data["market_data"].get("low_24h", {}).get("usd", "N/A"),  # And Highest price in 24h
-            "ath": data["market_data"].get("ath", {}).get("usd", "N/A"),  # The Highest price of all time
+            "price_change_percentage_24h": data.get("market_data", {}).get("price_change_percentage_24h", "N/A"), # Change in 24 hours
+            "market_cap": data.get("market_data", {}).get("market_cap", {}).get("usd", "N/A"),
+            "total_volume": data.get("market_data", {}).get("total_volume", {}).get("usd", "N/A"),
+            "total_supply": data.get("market_data", {}).get("total_supply", "N/A"),  # may be none
+            "max_supply": data.get("market_data", {}).get("max_supply", "N/A"),  # may be none
+            "circulating_supply": data.get("market_data", {}).get("circulating_supply", "N/A"),  # may be none
+            "fdv": data.get("market_data", {}).get("fully_diluted_valuation", {}).get("usd", "N/A"),
+            "high_24h": data.get("market_data", {}).get("high_24h", {}).get("usd", "N/A"),  # Lowest
+            "low_24h": data.get("market_data", {}).get("low_24h", {}).get("usd", "N/A"),  # And Highest price in 24h
+            "ath": data.get("market_data", {}).get("ath", {}).get("usd", "N/A"),  # The Highest price of all time
         }
 
         return Response(result)
